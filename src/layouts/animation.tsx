734300e61ece14c59c6209d3deba71406df09c41
yudai hiyama <yudai1204h@gmail.com>
2024-10-30 00:36:01 +0900

    fix

--- src/layouts/animation.tsx
+++
@@ -1,9 +0,0 @@
-import { Box } from "@chakra-ui/react";
-import { AnimationBase } from "@/components/AnimationBase";
-export default function Animation() {
-  return (
-    <Box width="100%" height="100lvh" overflow="hidden" position="relative">
-      <AnimationBase logo isDebug />
-    </Box>
-  );
-}
